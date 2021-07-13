312e318c6c53b77454f44aebc716a20ad9bdd6b6
DDeShon <36036284+DDeShon@users.noreply.github.com>
2021-07-13 09:30:08 -0500

    learning how to set up react

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const handleNameChange = () => {
+    const names = ["Bob", "Kevin", "Dave"];
+    const int = Math.floor(Math.random() * 3);
+    return names[int];
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Hello {handleNameChange()}!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
