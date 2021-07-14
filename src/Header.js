b66f58da62bc8467609cddf57292355afa7d0bcf
DDeShon <36036284+DDeShon@users.noreply.github.com>
2021-07-14 09:24:26 -0500

    learnign CSS with React

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,11 @@
 const Header = () => {
+  const headerStyle = {
+    backgroundColor: "mediumblue",
+    color: "#fff",
+  };
+
   return (
-    <header
-      style={{
-        backgroundColor: "mediumblue",
-        color: "#fff",
-      }}
-    >
+    <header style={headerStyle}>
       <h1>Groceries List</h1>
     </header>
   );
