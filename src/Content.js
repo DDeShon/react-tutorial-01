5b7008f6097c249ca6b05643e3c3e6317a6d354a
DDeShon <36036284+DDeShon@users.noreply.github.com>
2021-07-14 09:44:10 -0500

    learnign React click events

--- src/Content.js
+++ src/Content.js
@@ -4,9 +4,15 @@ const Content = () => {
     const int = Math.floor(Math.random() * 5);
     return names[int];
   };
+
+  const handleClick = () => {
+    console.log("You clicked it.");
+  };
+
   return (
     <main>
       <p>Hello {handleNameChange()}!</p>
+      <button onClick={handleClick}>Click It</button>
     </main>
   );
 };
