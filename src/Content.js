02a506824341e48ffbd299541736d3cc6181c586
DDeShon <36036284+DDeShon@users.noreply.github.com>
2021-07-16 09:26:16 -0500

    Learning React JS Lists and Keys

--- src/Content.js
+++ src/Content.js
@@ -38,7 +38,10 @@ const Content = () => {
               onChange={() => handleCheck(item.id)}
               checked={item.checked}
             />
-            <label onDoubleClick={() => handleCheck(item.id)}>
+            <label
+              style={item.checked ? { textDecoration: "line-through" } : null}
+              onDoubleClick={() => handleCheck(item.id)}
+            >
               {item.item}
             </label>
             <FaTrashAlt role="button" tabIndex="0" />
