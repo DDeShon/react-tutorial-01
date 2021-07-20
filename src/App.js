5519766cbb66154ab920cd55f47e6226c00e67e3
DDeShon <36036284+DDeShon@users.noreply.github.com>
2021-07-20 09:25:18 -0500

    Learning React JS Props and Prop Drilling

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Footer from "./Footer";
 function App() {
   return (
     <div className="App">
-      <Header title="Groceries" />
+      <Header />
       <Content />
       <Footer />
     </div>
